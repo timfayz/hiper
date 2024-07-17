67dddb40bf360b850b3c5f839efc88b54dc3aef2
Timur Fayzrakhmanov <tim.fayzrakhmanov@gmail.com>
2024-07-17 12:36:00 +0300

    Log: add an ability to define `log_fn` per scope

--- src/log.zig
+++ src/log.zig
@@ -61,12 +61,18 @@ pub fn scopeActive(tag: @TypeOf(.Enum)) bool {
 
 /// Initializes print functions under the `.tag` scope.
 /// Ensure the tag is present in `.log_scopes` for prints to function.
-pub fn scope(s: struct { tag: @TypeOf(.Enum), prefix: []const u8 = "" }) type {
+pub fn scope(
+    s: struct {
+        tag: @TypeOf(.Enum),
+        prefix: []const u8 = "",
+        log_fn: fn (comptime format: []const u8, args: anytype) void = defaults.log_fn,
+    },
+) type {
     return struct {
         /// Prints a formatted string using `.log_fn` within a given scope.
         pub fn print(comptime fmt: []const u8, args: anytype) void {
             if (!scopeActive(s.tag)) return;
-            defaults.log_fn(s.prefix ++ fmt, args);
+            s.log_fn(s.prefix ++ fmt, args);
         }
 
         /// Formats a string, splits it by lines, and prints each line within
@@ -77,14 +83,14 @@ pub fn scope(s: struct { tag: @TypeOf(.Enum), prefix: []const u8 = "" }) type {
             defer alloc.free(res);
             var iter = std.mem.splitScalar(u8, res, '\n');
             while (iter.next()) |line| {
-                defaults.log_fn(s.prefix ++ "{s}\n", .{line});
+                s.log_fn(s.prefix ++ "{s}\n", .{line});
             }
         }
 
         /// Prints a string using `.log_fn` within a given scope.
         pub fn printString(str: []const u8) void {
             if (!scopeActive(s.tag)) return;
-            defaults.log_fn(s.prefix ++ "{s}", .{str});
+            s.log_fn(s.prefix ++ "{s}", .{str});
         }
 
         /// Splits a string and prints each line within a given scope.
