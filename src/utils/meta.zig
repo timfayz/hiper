c02a0764f4a76f3881bd9cbde8fcd925c0bc369a
Timur Fayzrakhmanov <tim.fayzrakhmanov@gmail.com>
2024-12-25 16:48:17 +0300

    Meta: add `isInt()` and `isFloat()`

--- src/utils/meta.zig
+++ src/utils/meta.zig
@@ -5,6 +5,8 @@
 //! - isStruct()
 //! - isTuple()
 //! - isNum()
+//! - isInt()
+//! - isFloat()
 
 const std = @import("std");
 const meta = std.meta;
@@ -15,17 +17,17 @@ pub inline fn isStruct(arg: anytype) bool {
     return info == .@"struct" and !info.@"struct".is_tuple;
 }
 
+/// Checks comptime if `arg` is a tuple (structs are not considered tuples).
+pub inline fn isTuple(arg: anytype) bool {
+    const info = @typeInfo(@TypeOf(arg));
+    return info == .@"struct" and info.@"struct".is_tuple;
+}
+
 test isStruct {
     const equal = std.testing.expectEqual;
     try equal(false, isStruct(.{1}));
     try equal(false, isStruct(struct { usize }{1}));
     try equal(true, isStruct(struct { field: usize }{ .field = 1 }));
-}
-
-/// Checks comptime if `arg` is a tuple (structs are not considered tuples).
-pub inline fn isTuple(arg: anytype) bool {
-    const info = @typeInfo(@TypeOf(arg));
-    return info == .@"struct" and info.@"struct".is_tuple;
 }
 
 test isTuple {
@@ -46,10 +48,41 @@ pub inline fn isNum(arg: anytype) bool {
     };
 }
 
+/// Checks comptime if `arg` is an integer.
+pub inline fn isInt(arg: anytype) bool {
+    return switch (@typeInfo(@TypeOf(arg))) {
+        .int => true,
+        .comptime_int => true,
+        else => false,
+    };
+}
+
+/// Checks comptime if `arg` is a number (integer or float).
+pub inline fn isFloat(arg: anytype) bool {
+    return switch (@typeInfo(@TypeOf(arg))) {
+        .float => true,
+        .comptime_float => true,
+        else => false,
+    };
+}
+
 test isNum {
     const equal = std.testing.expectEqual;
     try equal(true, isNum(1));
     try equal(true, isNum(1.1));
     try equal(false, isNum(.{1}));
-    try equal(false, isNum(usize));
+}
+
+test isInt {
+    const equal = std.testing.expectEqual;
+    try equal(true, isInt(1));
+    try equal(false, isInt(1.1));
+    try equal(false, isInt(.{1}));
+}
+
+test isFloat {
+    const equal = std.testing.expectEqual;
+    try equal(false, isFloat(1));
+    try equal(true, isFloat(1.1));
+    try equal(false, isFloat(.{1}));
 }
